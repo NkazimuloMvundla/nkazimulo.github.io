9b50e89863137893e5403c925e5aa8e27590526f
Judgement1995 <45146228+Judgement1995@users.noreply.github.com>
2020-03-03 17:15:01 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(event){
   // array with texts to type in typewriter
-  var dataText = [ "Front End development", "Backend development", "CSS design", "I love building Web Apps", "nkazimulojudgement@gmail.com"];
+  var dataText = [ "Front End development", "Backend development", "CSS design", "I love building Web Apps"];
   
   // type one text in the typwriter
   // keeps calling itself until the text is finished
